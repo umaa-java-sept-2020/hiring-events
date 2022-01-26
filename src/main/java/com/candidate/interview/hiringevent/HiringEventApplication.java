12cb48990f9e011a62f1a5da23bfa408cfe56079
r0p06np <rabindra.kumar.patra@walmart.com>
2022-01-26 15:17:33 +0530

    hiring event security integration

--- src/main/java/com/candidate/interview/hiringevent/HiringEventApplication.java
+++ src/main/java/com/candidate/interview/hiringevent/HiringEventApplication.java
@@ -2,8 +2,11 @@ package com.candidate.interview.hiringevent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.candidate.interview.hiringevent","io.login.client"}
+)
 public class HiringEventApplication {
 
 	public static void main(String[] args) {
